e755755ade59b6d345f92bc944c0d608754417c3
Serge Voilokov <serge0x76@gmail.com>
2019-05-14 17:57:46 -0400

    Add functions

---
+++ init.go
@@ -0,0 +1,91 @@
+// Package autocomplete provides bash autocomplete for golang programs.
+package autocomplete
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+// Init ensures that .batchrc file contains autocomplete init statement.
+func Init(file, script string) {
+	if err := os.MkdirAll(filepath.Dir(file), 0755); err != nil {
+		panic(err)
+	}
+
+	actext, _ := ioutil.ReadFile(file)
+	if string(actext) != script {
+		if err := ioutil.WriteFile(file, []byte(script), 0644); err != nil {
+			panic(err)
+		}
+		fmt.Println("autocomplete installed. Run source ~/.bashrc now")
+	}
+
+	what := `for i in ~/.config/bash_completion/*; do source $i ; done`
+	bashrc := os.Getenv("HOME") + "/.bashrc"
+	buf, _ := ioutil.ReadFile(bashrc)
+	lines := strings.Split(string(buf), "\n")
+	found := false
+	for _, ln := range lines {
+		if ln == what {
+			found = true
+			break
+		}
+	}
+
+	if !found {
+		f, err := os.OpenFile(bashrc, os.O_RDWR|os.O_APPEND, 0660)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Fprintf(f, "\n%s\n", what)
+		f.Close()
+		fmt.Println("common autocomplete installed. Run source ~/.bashrc now")
+	}
+}
+
+// Handler is a function type for Handler function.
+type Handler func() []string
+
+var handlers = make(map[string]Handler)
+
+// Handle adds custom handler for the parameter. Handler should return list of possible completions.
+func Handle(name string, h Handler) {
+	_, ok := handlers[name]
+	if ok {
+		panic("handler is already assigned: " + name)
+	}
+	handlers[name] = h
+}
+
+// Print prints possible autocompletions.
+func Print() {
+	lastarg := flag.Arg(flag.NArg() - 1)
+	lastpar := flag.Arg(flag.NArg() - 2)
+
+	for name, handler := range handlers {
+		if lastarg != "-"+name {
+			continue
+		}
+		list := handler()
+		for _, s := range list {
+			if strings.HasPrefix(s, lastpar) {
+				fmt.Println(s)
+			}
+		}
+		return
+	}
+
+	flag.VisitAll(func(f *flag.Flag) {
+		if f.Name == "c" {
+			return
+		}
+		if strings.HasPrefix("-"+f.Name, flag.Arg(1)) {
+			fmt.Println("-" + f.Name)
+		}
+	})
+}
